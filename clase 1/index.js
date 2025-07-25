692dbb8b563319145900b525ec062085e4d2008e
Alexander-LP-03 <edwin.1103alexander@gmail.com>
2025-07-24 19:15:13 -0600

    subiendo clase 1

---
+++ clase 1/index.js
@@ -0,0 +1,26 @@
+let name ="Alexander";
+let alias = "Alex";
+let email = "edwin.1103alexander@gmai.lcom";
+console.log("hola" + name + "como estas?")
+
+//tipo de datos number
+let age = 37;//entero
+let altura = 27.50;// flotante
+
+//tipo de dato nooleano
+let isStudent = false;
+let isTeacher = true;
+
+///tipo de dato Undefined
+let valorUnDefined // no tiene valor asignado
+console.log(valorUnDefined)
+
+//tipo de dato null
+let nulValue = null;
+
+//tipo de datos symbol
+let mySymbol = Symbol("mySymbol");
+
+//tipo de dATO BigInt
+let myBigInt = BigInt(2345543234567823456789093456787658954)
+let myBig = 245653456533456n;
---
+++ clase 1/variable.js
@@ -0,0 +1,12 @@
+var helloWorld = "Hello, World!";
+var greeting = "Welcome to the JavaScript world!";
+console.log(helloWorld);
+
+//variable let
+//let helloWord = "HOLA MUNDO";
+//helloWord = "esto es un nuevo mensaje"
+//console.log(helloWord);
+
+//constante const
+const helloWord = "hola mundo"
+console.log(helloWord)
