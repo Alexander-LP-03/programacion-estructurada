ed0e79275bed84b59c55cc09a3ffc53c62d6bc4a
Alexander-LP-03 <edwin.1103alexander@gmail.com>
2025-07-28 10:28:55 -0600

    añadiendo clase 2

---
+++ clase 5/ejemplo1.js
@@ -0,0 +1,15 @@
+//IMPORTAR EL MODULO
+//const readline = require('readline');
+import readline from 'readline';
+
+//CREAR UNA INTERFAZ
+const rl = readline.createInterface({ 
+input: process.stdin,
+output: process.stdout
+})
+
+//REALIZAR PREGUNTA AL USUARIO
+rl.question('¿Cual es tu nombre? ', (nombre) => {
+    console.log(`Hola, buen dia ${nombre}`);
+    rl.close(); //CERRAR LA INTERFAZ    
+})
