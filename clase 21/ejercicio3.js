c13fd4ea093df0d8a391b9923f5210ead10f2a9c
Alexander-LP-03 <edwin.1103alexander@gmail.com>
2025-09-29 15:32:17 -0600

    Resuelto

---
+++ clase 21/ejercicio3.js
@@ -0,0 +1,21 @@
+import readline from 'readline';
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const temperaturas = [];
+
+rl.question("Ingrese la temperatura del dia 1: ", (temp1) => {
+    temperaturas.push(temp1);
+    rl.question("Ingrese la temperatura del dia 2: ", (temp2) => {
+        temperaturas.push(temp2);
+        rl.question("Ingrese la temperatura del dia 3: ", (temp3) => {
+            temperaturas.push(temp3);
+
+            const tempMax = Math.max(parseFloat(temperaturas[0]), parseFloat(temperaturas[1]), parseFloat(temperaturas[2]));
+            console.log(`\nLa temperatra mas alta en los tres dias es: ${tempMax}`);
+            rl.close();
+        })
+    })
+})
